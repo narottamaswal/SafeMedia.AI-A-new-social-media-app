73a72d14ab298bc6be234a94e83ebd7afa734e1a
Narottam Aswal <narottam98678@gmail.com>
2021-05-05 12:36:47 +0530

    message

---
+++ FirebaseApi.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnHs48r4q3spJBzAf4aHFKk1hdqzSmaj0",
+    authDomain: "soy-audio-260814.firebaseapp.com",
+    databaseURL: "https://soy-audio-260814.firebaseio.com",
+    projectId: "soy-audio-260814",
+    storageBucket: "soy-audio-260814.appspot.com",
+    messagingSenderId: "233848809744",
+    appId: "1:233848809744:web:0900d3a16104f4ea819d95",
+    measurementId: "G-65C0CT3WTY"
+  };
+if (!firebase.apps.length) {
+  var firebaseDb = firebase.initializeApp(firebaseConfig);
+}
+
+var db = firebaseDb.firestore();
+export {db};
